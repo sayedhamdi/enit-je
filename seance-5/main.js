f7363a15bacc0c7dd46fcd8b466b9a20ce6b8fd6
sayed <tkdsayed@gmail.com>
2023-05-12 20:46:15 +0100

    js

---
+++ seance-5/main.js
@@ -0,0 +1,35 @@
+let x = 6
+
+if(x > 5){
+    console.log("x akber men 5")
+}else{
+    console.log("x 9ad wala asgher men 5")
+}
+
+
+for(let i=0;i<=10;i++){
+    console.log("i tawa : " +i )
+}
+
+let age = 15;
+
+
+
+let name = 'mohsen'
+
+
+let reversed = ''
+
+
+for(let i=0;i<name.length;i++){
+    reversed = name[i] + reversed
+}
+
+
+console.log(reversed)
+
+let tInt = name.split('')
+console.log(tInt.reverse().join(''))
+/* 
+
+,hv,hvg*/
